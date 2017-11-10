419642c6d8250ec27a2048fc783b7f87aa7c180d
Mustafa Dokumacı <mustafa.dokumaci@capgemini.com>
2017-11-10 11:08:57 +0100

    start proper testing

--- test/index.js
+++ test/index.js
@@ -1,34 +1,105 @@
+const test = require('tape')
 const { Struct } = require('../')
 
-const master = new Struct()
+test('set - get', t => {
+  const master = new Struct()
 
-master.set({
-  deep: {
-    real: 'thing'
-  },
-  pointers: {
-    pointer1: ['@', 'deep'],
-    pointer2: ['@', 'deep', 'real'],
-    pointer3: ['@', 'pointers', 'pointer1'],
-    pointer4: ['@', 'pointers', 'pointer2']
-  }
+  master.set({
+    deep: {
+      real: 'thing'
+    },
+    pointers: {
+      pointer1: ['@', 'deep'],
+      pointer2: ['@', 'deep', 'real'],
+      pointer3: ['@', 'pointers', 'pointer1'],
+      pointer4: ['@', 'pointers', 'pointer2']
+    }
+  })
+
+  t.equals(
+    master.get(['pointers', 'pointer1', 'real']).compute(),
+    'thing',
+    'pointers.pointer1.real.compute() = thing'
+  )
+  t.equals(
+    master.get(['pointers', 'pointer2']).compute(),
+    'thing',
+    'pointers.pointer2.compute() = thing'
+  )
+  t.equals(
+    master.get(['pointers', 'pointer3', 'real']).compute(),
+    'thing',
+    'pointers.pointer3.real.compute() = thing'
+  )
+  t.equals(
+    master.get(['pointers', 'pointer4']).compute(),
+    'thing',
+    'pointers.pointer4.compute() = thing'
+  )
+  t.equals(
+    master.inspect(),
+    'Struct { deep, pointers }',
+    'master.inspect() = Struct { deep, pointers }'
+  )
+  t.equals(
+    master.get('pointers').inspect(),
+    'Struct pointers { pointer1, pointer2, pointer3, pointer4 }',
+    'master.pointers.inspect() = Struct pointers { pointer1, pointer2, pointer3, pointer4 }'
+  )
+  t.equals(
+    master.get(['pointers', 'pointer2']).inspect(),
+    'Struct pointer2 { val: Struct real { val: thing } }',
+    'master.pointers.pointer2.inspect() = Struct pointer2 { val: Struct real { val: thing } }'
+  )
+  t.same(
+    master.get(['deep', 'real']).path(),
+    [ 'deep', 'real' ],
+    'master.deep.real.path() = [ \'deep\', \'real\' ]'
+  )
+  t.same(
+    master.serialize(),
+    {
+      deep: { real: 'thing' },
+      pointers: {
+        pointer1: [ '@', 'deep' ],
+        pointer2: [ '@', 'deep', 'real' ],
+        pointer3: [ '@', 'pointers', 'pointer1' ],
+        pointer4: [ '@', 'pointers', 'pointer2' ]
+      }
+    },
+    'master.pointers.serialize() = good'
+  )
+  t.same(
+    master.get('pointers').serialize(),
+    {
+      pointer1: [ '@', 'deep' ],
+      pointer2: [ '@', 'deep', 'real' ],
+      pointer3: [ '@', 'pointers', 'pointer1' ],
+      pointer4: [ '@', 'pointers', 'pointer2' ]
+    },
+    'master.pointers.serialize() = good'
+  )
+  t.same(
+    master.get('deep').serialize(),
+    { real: 'thing' },
+    'master.pointers.serialize() = { real: \'thing\' }'
+  )
+  t.equals(
+    master.get('pointers').get('pointer5', ['@', 'pointers', 'pointer1']).origin().get('real').compute(),
+    'thing',
+    'master.pointers.pointer5.origin().real.compute() = thing'
+  )
+
+  master.get(['pointers', 'pointer6'], ['@', 'pointers', 'pointer5'])
+
+  t.equals(
+    master.get(['pointers', 'pointer6', 'real']).compute(),
+    'thing',
+    'master.pointers.pointer6.real.compute() = thing'
+  )
+
+  t.end()
 })
-
-console.log('compute pointer1/real:', master.get(['pointers', 'pointer1', 'real']).compute())
-console.log('compute pointer2:', master.get(['pointers', 'pointer2']).compute())
-console.log('compute pointer3/real:', master.get(['pointers', 'pointer3', 'real']).compute())
-console.log('compute pointer4:', master.get(['pointers', 'pointer4']).compute())
-console.log('inspect master:', master)
-console.log('inspect pointers:', master.get('pointers'))
-console.log('inspect pointer2:', master.get(['pointers', 'pointer2']))
-console.log('path of deep/real:', master.get(['deep', 'real']).path())
-console.log('serialize pointers:', master.get(['pointers']).serialize())
-console.log('serialize deep:', master.get(['deep']).serialize())
-console.log('serialize master:', master.serialize())
-
-console.log('compute pointer5/real:', master.get('pointers').get('pointer5', ['@', 'pointers', 'pointer1']).origin().get('real').compute())
-master.get(['pointers', 'pointer6'], ['@', 'pointers', 'pointer5'])
-console.log('compute pointer6/real:', master.get(['pointers', 'pointer6', 'real']).compute())
 
 const state = new Struct()
 

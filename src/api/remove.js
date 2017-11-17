10e555aef968604df17b07de7da41c4d9f65f837
Mustafa Dokumacı <mustafa@foraa.com>
2017-11-17 17:19:50 +0100

    start implementing remove

--- src/api/remove.js
+++ src/api/remove.js
@@ -1,19 +1,25 @@
 import { Leaf } from '../index'
 
-const removeFromParent = (branch, leaf, id, stamp) => {
-
+const removeFromParent = (parent, id, stamp) => {
+  parent.keys.splice(
+    parent.keys.indexOf(id), 1
+  )
 }
 
-const remove = (branch, leaf, stamp) => {
-  if (leaf.struct === branch) {
-    delete branch.leaves[leaf.id]
-  } else {
-    branch.leaves[leaf.id] = new Leaf(
-      branch, leaf.id, void 0, stamp, leaf.p, leaf.key
-    )
-    branch.leaves[leaf.id].val = null
-  }
+const removeOwn = (branch, leaf, stamp) => {
+  delete branch.leaves[leaf.id]
 
+  removeFromParent(branch.leaves[leaf.p], leaf.id, stamp)
+}
+
+const removeBranch = (branch, leaf, stamp) => {
+  branch.leaves[leaf.id] = new Leaf(
+    branch, leaf.id, void 0, stamp, leaf.p, leaf.key
+  )
+  branch.leaves[leaf.id].val = null
+}
+
+const removeChildren = (branch, leaf, stamp) => {
   if (leaf.keys) {
     leaf.keys.forEach(keyId => {
       if (branch.leaves[keyId]) {
@@ -21,6 +27,16 @@ const remove = (branch, leaf, stamp) => {
       }
     })
   }
+}
+
+const remove = (branch, leaf, stamp) => {
+  if (leaf.struct === branch) {
+    removeOwn(branch, leaf, stamp)
+  } else {
+    removeBranch(branch, leaf, stamp)
+  }
+
+  removeChildren(branch, leaf, stamp)
 }
 
 export { remove }

988807df97964ebac2d60696583f244a8a70ea15
Mustafa Dokumacı <mustafa@foraa.com>
2018-01-09 23:18:49 +0100

    simplify

--- src/api/listeners/emit.js
+++ src/api/listeners/emit.js
@@ -54,12 +54,7 @@ const subscriptions = (branch, id, stamp, subs) => {
       emitReferenceSubscriptions(branch, id, stamp, subs)
     }
 
-    const leaf = getFromLeaves(branch, id).leaves[id]
-    if (leaf.parent) {
-      id = leaf.parent
-    } else {
-      return
-    }
+    id = getFromLeaves(branch, id).leaves[id].parent
   }
 }
 

aa88bb5cba645d5b3cd789f01f6d57ed3ba58153
Mustafa Dokumacı <mustafa@foraa.com>
2018-01-13 14:33:01 +0100

    clean up test

--- test/subscriptions.js
+++ test/subscriptions.js
@@ -265,6 +265,11 @@ test('subscriptions - deep field references', t => {
 })
 
 test('subscriptions - circular references', t => {
+  const branch1Fire = []
+  const branch2Fire = []
+  const branch21Fire = []
+  const branch22Fire = []
+
   const master = create({
     id: 'master',
     list: {
@@ -314,101 +319,54 @@ test('subscriptions - circular references', t => {
 
   branch1.get('ref').subscribe(item => {
     if (item.get('id') === void 0) {
+      branch1Fire.push('branch1.initial')
       t.pass('branch1 initial fire')
     } else if (item.get('id').compute() === 'i1') {
-      t.equals(
-        item.get([ 'items', 'sub1', 'bf1' ]).compute(), false,
-        'branch1.i1.bf1 fired for false'
-      )
-      t.equals(
-        item.get([ 'items', 'sub2', 'bf2' ]).compute(), false,
-        'branch1.i1.sub2.bf2 fired for false'
-      )
+      const val1 = item.get([ 'items', 'sub1', 'bf1' ]).compute()
+      branch1Fire.push(`branch1.i1.items.sub1.bf1=${val1}`)
+      const val2 = item.get([ 'items', 'sub2', 'bf2' ]).compute()
+      branch1Fire.push(`branch1.i1.items.sub2.bf2=${val2}`)
     } else if (item.get('id').compute() === 'i3') {
-      t.equals(
-        item.get([ 'items', 'sub2', 'bf2' ]).compute(), false,
-        'branch1.i3.sub2.bf2 fired for false'
-      )
-      t.equals(
-        item.get([ 'items', 'sub4', 'sub', 'bf4' ]).compute(), true,
-        'branch1.i3.sub4.sub.bf4 fired for false'
-      )
-      t.equals(
-        item.get([ 'items', 'sub4', 'f1' ]).compute(), 'v1',
-        'branch1.i3.sub4.f1 fired for v1'
-      )
+      const val1 = item.get([ 'items', 'sub2', 'bf2' ]).compute()
+      branch1Fire.push(`branch1.i3.items.sub2.bf2=${val1}`)
+      const val2 = item.get([ 'items', 'sub4', 'sub', 'bf4' ]).compute()
+      branch1Fire.push(`branch1.i3.items.sub4.sub.bf4=${val2}`)
     }
   })
 
   branch2.get('ref').subscribe(item => {
     if (item.get('id') === void 0) {
-      t.pass('branch2 initial fire')
+      branch2Fire.push('branch2.initial')
     } else if (item.get('id').compute() === 'i2') {
-      t.equals(
-        item.get([ 'items', 'sub3', 'bf3' ]).compute(), false,
-        'branch2.i2.sub3.bf3 fired for false'
-      )
-      t.equals(
-        item.get([ 'items', 'sub4', 'f1' ]).compute(), true,
-        'branch2.i2.sub4.f1 fired for true'
-      )
+      const val = item.get([ 'items', 'sub3', 'bf3' ]).compute()
+      branch2Fire.push(`branch2.i2.items.sub3.bf3=${val}`)
     } else if (item.get('id').compute() === 'i3') {
-      t.equals(
-        item.get([ 'items', 'sub2', 'items', 'sub3', 'bf3' ]).compute(), false,
-        'branch2.i3.sub2.items.sub3.bf3 fired for false'
-      )
-      t.equals(
-        item.get([ 'items', 'sub4', 'f1' ]).compute(), true,
-        'branch2.i3.sub4.f1 fired for true'
-      )
+      const val = item.get([ 'items', 'sub2', 'items', 'sub3', 'bf3' ]).compute()
+      branch2Fire.push(`branch2.i3.items.sub2.items.sub3.bf3=${val}`)
     }
   })
 
   branch21.get('ref').subscribe(item => {
     if (item.get('id') === void 0) {
-      t.pass('branch21 initial fire')
+      branch21Fire.push('branch21.initial')
     } else if (item.get('id').compute() === 'i2') {
-      t.equals(
-        item.get([ 'items', 'sub3', 'bf3' ]).compute(), false,
-        'branch21.i2.sub3.bf3 fired for false'
-      )
-      t.equals(
-        item.get([ 'items', 'sub4', 'f1' ]).compute(), true,
-        'branch21.i2.sub4.f1 fired for true'
-      )
+      const val = item.get([ 'items', 'sub4', 'f1' ]).compute()
+      branch21Fire.push(`branch21.i2.items.sub4.f1=${val}`)
     } else if (item.get('id').compute() === 'i3') {
-      t.equals(
-        item.get([ 'items', 'sub2', 'items', 'sub3', 'bf3' ]).compute(), false,
-        'branch21.i3.sub2.items.sub3.bf3 fired for false'
-      )
-      t.equals(
-        item.get([ 'items', 'sub4', 'f1' ]).compute(), true,
-        'branch21.i3.sub4.f1 fired for true'
-      )
+      const val = item.get([ 'items', 'sub4', 'f1' ]).compute()
+      branch21Fire.push(`branch21.i3.items.sub4.f1=${val}`)
     }
   })
 
   branch22.get('ref').subscribe(item => {
     if (item.get('id') === void 0) {
-      t.pass('branch22 initial fire')
+      branch22Fire.push('branch22.initial')
     } else if (item.get('id').compute() === 'i2') {
-      t.equals(
-        item.get([ 'items', 'sub3', 'bf3' ]).compute(), false,
-        'branch21.i2.sub3.bf3 fired for false'
-      )
-      t.equals(
-        item.get([ 'items', 'sub4', 'f1' ]).compute(), true,
-        'branch21.i2.sub4.f1 fired for true'
-      )
+      const val =item.get([ 'items', 'sub3', 'bf3' ]).compute()
+      branch22Fire.push(`branch22.i2.items.sub3.bf3=${val}`)
     } else if (item.get('id').compute() === 'i1') {
-      t.equals(
-        item.get([ 'items', 'sub2', 'items', 'sub3', 'bf3' ]).compute(), false,
-        'branch21.i3.sub2.items.sub3.bf3 fired for false'
-      )
-      t.equals(
-        item.get([ 'items', 'sub3', 'items', 'sub4', 'f1' ]).compute(), true,
-        'branch21.i3.sub3.items.sub4.f1 fired for true'
-      )
+      const val = item.get([ 'items', 'sub2', 'items', 'sub3', 'bf3' ]).compute()
+      branch22Fire.push(`branch22.i1.items.sub2.items.sub3.bf3=${val}`)
     }
   })
 
@@ -432,6 +390,45 @@ test('subscriptions - circular references', t => {
     ref: ['@', 'list', 'i2']
   })
 
+  t.same(
+    branch1Fire,
+    [
+      'branch1.initial',
+      'branch1.i1.items.sub1.bf1=false',
+      'branch1.i1.items.sub2.bf2=false'
+    ],
+    'branch1Fire = correct'
+  )
+  t.same(
+    branch2Fire,
+    [
+      'branch2.initial',
+      'branch2.i2.items.sub3.bf3=false'
+    ],
+    'branch2Fire = correct'
+  )
+  t.same(
+    branch21Fire,
+    [
+      'branch21.initial',
+      'branch21.i2.items.sub4.f1=true'
+    ],
+    'branch21Fire = correct'
+  )
+  t.same(
+    branch22Fire,
+    [
+      'branch22.initial',
+      'branch22.i2.items.sub3.bf3=false'
+    ],
+    'branch22Fire = correct'
+  )
+
+  branch1Fire.length = 0
+  branch2Fire.length = 0
+  branch21Fire.length = 0
+  branch22Fire.length = 0
+
   branch21.set({
     ref: [ '@', 'list', 'i3' ]
   })
@@ -447,6 +444,30 @@ test('subscriptions - circular references', t => {
   branch2.set({
     ref: ['@', 'list', 'i3']
   })
+
+  t.same(
+    branch1Fire,
+    [
+      'branch1.i3.items.sub2.bf2=false',
+      'branch1.i3.items.sub4.sub.bf4=true'
+    ],
+    'branch1Fire = correct'
+  )
+  t.same(
+    branch2Fire,
+    [ 'branch2.i3.items.sub2.items.sub3.bf3=false' ],
+    'branch2Fire = [ branch2.i3.items.sub2.items.sub3.bf3=false ]'
+  )
+  t.same(
+    branch21Fire,
+    [ 'branch21.i3.items.sub4.f1=true' ],
+    'branch21Fire = [ branch21.i3.items.sub4.f1=true ]'
+  )
+  t.same(
+    branch22Fire,
+    [ 'branch22.i1.items.sub2.items.sub3.bf3=false' ],
+    'branch22Fire = [ branch22.i1.items.sub2.items.sub3.bf3=false ]'
+  )
 
   t.end()
 })

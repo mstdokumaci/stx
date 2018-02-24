8cefbbf0993bf8a0866309866940b32947e33531
Mustafa Dokumacı <mustafa@foraa.com>
2018-02-24 23:13:09 +0100

    implement stamp offset adjusting

--- src/api/client/incoming.js
+++ src/api/client/incoming.js
@@ -1,8 +1,6 @@
+import { setOffset } from '../../stamp'
+
 const heartbeatTimeout = 3e3
-
-const adjustStamp = (branch, stamp) => {
-
-}
 
 const heartbeat = branch => {
   const socket = branch.client.socket
@@ -29,7 +27,7 @@ const incoming = (branch, data) => {
   const { t: stamp, h: heartbeat, e: emits, l: leaves } = data
 
   if (stamp) {
-    adjustStamp(branch, stamp)
+    setOffset(branch.stamp, stamp)
   }
 
   if (heartbeat) {

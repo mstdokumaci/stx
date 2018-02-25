167d04db38b974f8d085aa9a529fd79f027b8660
Mustafa Dokumacı <mustafa@foraa.com>
2018-02-25 12:27:02 +0100

    test reconnect

--- test/network/listen-connect.js
+++ test/network/listen-connect.js
@@ -1,7 +1,7 @@
 const test = require('tape')
 const { create } = require('../../dist/index')
 
-test('network - server1', t => {
+test('network - listen & connect', t => {
   const master = create()
   const server = master.listen(7070)
 
@@ -16,17 +16,22 @@ test('network - server1', t => {
   t.end()
 })
 
-test('network - server2', t => {
+test('network - listen & reconnect', t => {
   const master = create()
-  const server = master.listen(7070)
+  const server1 = master.listen(7070)
 
   const client = create()
   const socket = client.connect('ws://localhost:7070')
+
+  server1.close()
+  t.pass('server1 closed')
+  const server2 = master.listen(7070)
+
   socket.close()
   t.pass('socket closed')
 
-  server.close()
-  t.pass('server closed')
+  server2.close()
+  t.pass('server2 closed')
 
   t.end()
 })

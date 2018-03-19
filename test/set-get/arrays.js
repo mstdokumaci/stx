0775b31315445c37636e5e51d9810ed448072812
Mustafa Dokumacı <mustafa@foraa.com>
2018-03-19 14:04:39 +0100

    split set-get tests into files

---
+++ test/set-get/arrays.js
@@ -0,0 +1,112 @@
+const test = require('tape')
+const { create, createStamp } = require('../../dist/index')
+
+test('set - get - arrays', t => {
+  const master = create()
+  const stamp = createStamp()
+
+  master.set({
+    deep: {
+      real: [ 1, 2, 3 ]
+    },
+    pointers: {
+      pointer1: [ '@', 'deep', 'real' ],
+      pointer2: [ '@', 'pointers', 'pointer1' ]
+    },
+    dummy: null
+  }, stamp)
+
+  t.same(
+    master.get([ 'pointers', 'pointer1' ]).compute(),
+    [ 1, 2, 3 ],
+    'pointers.pointer1.compute() = [ 1, 2, 3 ]'
+  )
+  t.same(
+    master.get([ 'pointers', 'pointer2' ]).compute(),
+    [ 1, 2, 3 ],
+    'pointers.pointer2.compute() = [ 1, 2, 3 ]'
+  )
+  t.equals(
+    master.inspect(),
+    'Struct { deep, pointers }',
+    'master.inspect() = Struct { deep, pointers }'
+  )
+  t.same(
+    master.serialize(),
+    {
+      deep: {
+        real: [ 1, 2, 3 ]
+      },
+      pointers: {
+        pointer1: [ '@', 'deep', 'real' ],
+        pointer2: [ '@', 'pointers', 'pointer1' ]
+      }
+    },
+    'master.pointers.serialize() = correct'
+  )
+  t.same(
+    master.get([ 'deep', 'real' ]).inspect(),
+    'Struct real { val: 1,2,3 }',
+    'master.deep.real.inspect() = Struct real { val: 1,2,3 }'
+  )
+
+  const branch1 = master.create({
+    deep: {
+      real: [ 3, 2, 1 ],
+      other: {}
+    }
+  })
+
+  master.set({
+    deep: {
+      real: [ 2, 3, 1 ],
+      other: {}
+    }
+  })
+
+  t.same(
+    master.get([ 'pointers', 'pointer2' ]).compute(),
+    [ 2, 3, 1 ],
+    'branch1.pointers.pointer2.compute() = [ 2, 3, 1 ]'
+  )
+  t.same(
+    master.serialize(),
+    {
+      deep: {
+        other: {},
+        real: [ 2, 3, 1 ]
+      },
+      pointers: {
+        pointer1: [ '@', 'deep', 'real' ],
+        pointer2: [ '@', 'pointers', 'pointer1' ]
+      }
+    },
+    'master.serialize() = correct'
+  )
+  t.equals(
+    master.get([ 'deep', 'other' ]).inspect(),
+    'Struct other { }',
+    'master.deep.other.inspect() = Struct other { }'
+  )
+  t.same(
+    branch1.get([ 'pointers', 'pointer2' ]).compute(),
+    [ 3, 2, 1 ],
+    'branch1.pointers.pointer2.compute() = [ 3, 2, 1 ]'
+  )
+  t.same(
+    branch1.serialize(),
+    {
+      deep: {
+        other: {},
+        real: [ 3, 2, 1 ]
+      },
+      pointers: {
+        pointer1: [ '@', 'deep', 'real' ],
+        pointer2: [ '@', 'pointers', 'pointer1' ]
+      }
+    },
+    'branch1.serialize() = correct'
+  )
+
+  t.end()
+})
---
+++ test/set-get/corner-cases.js
@@ -0,0 +1,132 @@
+const test = require('tape')
+const { create, createStamp } = require('../../dist/index')
+
+test('interference', t => {
+  const stamp = createStamp()
+  const master = create({
+    deep: {
+      real: 'thing'
+    }
+  }, stamp)
+
+  const branch1 = master.create()
+  const branch2 = branch1.create()
+
+  const b1Real = branch1.get([ 'deep', 'real' ])
+  const b2Deep = branch2.get([ 'deep', 'real' ]).parent()
+
+  b1Real.set('override1')
+  b2Deep.set({ real: 'override2' })
+
+  t.equals(
+    master.get([ 'deep', 'real' ]).compute(),
+    'thing',
+    'master.deep.real = thing'
+  )
+  t.equals(
+    branch1.get([ 'deep', 'real' ]).compute(),
+    'override1',
+    'branch1.deep.real = override1'
+  )
+  t.equals(
+    branch2.get([ 'deep', 'real' ]).compute(),
+    'override2',
+    'branch2.deep.real = override2'
+  )
+
+  t.end()
+})
+
+test('root operations', t => {
+  const master = create({
+    real: 'thing'
+  })
+
+  t.equals(
+    master.parent(),
+    void 0,
+    'root does not have parent'
+  )
+
+  master.set(null)
+
+  t.same(
+    master.serialize(),
+    {},
+    'remove on root works'
+  )
+
+  t.end()
+})
+
+test('create from leaf', t => {
+  const master = create({
+    deep: {
+      real: 'thing'
+    }
+  })
+
+  try {
+    const branch1 = master.get(['deep', 'real']).create()
+    branch1.set('override')
+  } catch (error) {
+    t.equals(error.message, 'Can not create from leaf', 'Can not create from leaf')
+  }
+
+  t.end()
+})
+
+test('do not set in reference get', t => {
+  const master = create({
+    key1: {
+      key2: {
+        key3: 'thing'
+      }
+    },
+    pointer: [ '@', 'key1' ]
+  })
+
+  master.get([ 'pointer', 'key2', 'key31' ], 'thing2')
+  master.get([ 'pointer', 'key22', 'key32' ], 'thing3')
+
+  t.same(
+    master.serialize(),
+    {
+      key1: {
+        key2: {
+          key3: 'thing',
+          key31: 'thing2'
+        }
+      },
+      pointer: {
+        key22: {
+          key32: 'thing3'
+        },
+        val: [ '@', 'key1' ]
+      }
+    },
+    'master.serialize() = correct'
+  )
+
+  t.end()
+})
+
+test('ignore same val in branch', t => {
+  const master = create({
+    real: 'thing'
+  })
+
+  const branch1 = master.create({
+    real: 'thing'
+  })
+
+  master.get('real').set('updated')
+
+  t.equals(
+    branch1.get('real').compute(),
+    'updated',
+    'branch1.real.compute() = updated'
+  )
+
+  t.end()
+})

2e88271288eb510052d63f21c17a2f42c7f40490
Mustafa Dokumacı <mustafa@foraa.com>
2019-01-02 16:37:29 +0100

    add test for non existing subscription

---
+++ test/network/subscriptions/corner-cases.js
@@ -0,0 +1,36 @@
+const test = require('tape')
+const { create } = require('../../../dist')
+
+test('network - subscriptions - corner cases', t => {
+  const sMaster = create({
+    existing: 'value'
+  })
+
+  const server = sMaster.listen(7070)
+
+  const cMaster = create()
+
+  cMaster.get('non-existing', {}).subscribe(
+    ne => {
+      if (ne.compute()) {
+        t.fail('should not fire for non existing')
+      }
+    }
+  )
+  cMaster.get('existing', {}).subscribe(
+    existing => {
+      if (existing.compute()) {
+        t.equals(
+          existing.compute(),
+          'value',
+          'existing.compute() === value'
+        )
+        client.socket.close()
+        server.close()
+        t.end()
+      }
+    }
+  )
+
+  const client = cMaster.connect('ws://localhost:7070')
+})

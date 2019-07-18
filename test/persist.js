ad7849ffaefb546ffffab32f8e1e99f609f019fb
Mustafa Dokumaci <mustafa@foraa.com>
2019-07-18 16:02:19 +0200

    test reference persistence

--- test/persist.js
+++ test/persist.js
@@ -71,3 +71,55 @@ test('persist - basic get after delete', async t => {
 
   t.end()
 })
+
+test('persist - set with reference', async t => {
+  const persist = new Persist('state-ref')
+  const state = await createPersist(
+    { key1: 'value1' }, persist
+  )
+
+  state.set({
+    key2: 'value 2',
+    pointer: [ '@', 'key1' ]
+  })
+
+  await persist.stop()
+
+  t.end()
+})
+
+test('persist - get with reference and update', async t => {
+  const persist = new Persist('state-ref')
+  const state = await createPersist({}, persist)
+
+  const listener = state.get('pointer').on((type, _, item) => {
+    t.equals(
+      `${type} ${item.compute()}`,
+      'set updated 1',
+      'set updated 1'
+    )
+
+    listener.off()
+
+    state.set({ pointer: [ '@', 'key2' ] })
+
+    t.end()
+  })
+
+  state.set({ key1: 'updated 1' })
+})
+
+test('persist - get with reference after update', async t => {
+  const persist = new Persist('state-ref')
+  const state = await createPersist({}, persist)
+
+  state.get('pointer').on((type, _, item) => {
+    t.equals(
+      `${type} ${item.compute()}`, 'set updated 2', 'set updated 2'
+    )
+
+    t.end()
+  })
+
+  state.set({ key2: 'updated 2' })
+})

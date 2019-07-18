5064e20b22181ccec67494758e6669b952a26154
Mustafa Dokumaci <mustafa@foraa.com>
2019-07-18 22:46:08 +0200

    add network persist test

---
+++ test/network/persist.js
@@ -0,0 +1,93 @@
+const test = require('tape')
+const { create, createPersist } = require('../../dist')
+const { Persist } = require('../helpers')
+
+test('network - persist set', async t => {
+  const masterPersist = new Persist('master')
+  const sMaster = await createPersist({
+    clients: 0,
+    first: {
+      id: 1
+    },
+    second: {
+      id: 2
+    },
+    pointer: [ '@', 'first' ]
+  }, masterPersist)
+
+  const server = sMaster.listen(7070)
+
+  server.switchBranch = async (_, branchKey, switcher) => {
+    const branchPersist = new Persist(branchKey)
+    const toBranch = await switcher(branchKey, branchPersist)
+
+    toBranch.branch.clientCanUpdate = [
+      {
+        path: [ 'pointer' ]
+      }
+    ]
+  }
+
+  const cMaster = create()
+
+  cMaster.on('connected', val => {
+    if (val) {
+      const sub = cMaster.get('pointer', {}).subscribe(pointer => {
+        const id = pointer.get('id')
+        if (id) {
+          t.same(id.compute(), 1, 'id.compute() = 1')
+
+          sub.unsubscribe()
+
+          pointer.set([ '@', 'second' ])
+
+          setTimeout(() => client.socket.close())
+        }
+      })
+
+      cMaster.switchBranch('A')
+    } else {
+      server.close()
+      t.end()
+    }
+  })
+
+  const client = cMaster.connect('ws://localhost:7070')
+})
+
+test('network - persist get', async t => {
+  const masterPersist = new Persist('master')
+  const sMaster = await createPersist({}, masterPersist)
+
+  const server = sMaster.listen(7070)
+
+  server.switchBranch = async (fromBranch, branchKey, switcher) => {
+    return switcher(branchKey, new Persist(branchKey))
+  }
+
+  const cMaster = create()
+
+  cMaster.on('connected', val => {
+    if (val) {
+      cMaster.get('pointer', {}).subscribe(pointer => {
+        const id = pointer.get('id')
+        if (id) {
+          if (id.compute() === 2) {
+            t.pass('id.compute() === 2')
+            sMaster.get([ 'second', 'id' ]).set('updated 2')
+          } else if (id.compute() === 'updated 2') {
+            t.pass('id.compute() === updated 2')
+            client.socket.close()
+          }
+        }
+      })
+
+      cMaster.switchBranch('A')
+    } else {
+      server.close()
+      t.end()
+    }
+  })
+
+  const client = cMaster.connect('ws://localhost:7070')
+})

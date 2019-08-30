415914d212d69cd253722f96a3b426013e3654b1
Mustafa Dokumaci <mustafa@foraa.com>
2019-08-30 16:42:28 +0200

    get rid of complicated children function

--- src/api/array.js
+++ src/api/array.js
@@ -2,69 +2,61 @@ import { Leaf } from '../leaf'
 import { getString } from '../cache'
 import { compute } from './compute'
 
-const children = (branch, id, cb) => {
-  const keys = []
-  let found
+const forEach = (branch, id, cb) => {
   for (const key in branch.leaves[id].keys) {
     if (branch.leaves[key] !== null) {
-      if (cb) {
-        if (cb(key)) {
-          found = key
-          break
-        }
-      } else {
-        keys.push(key)
-      }
+      cb(new Leaf(branch, key), getString(branch.leaves[key].key))
     }
   }
-  return found || keys
-}
-
-const forEach = (branch, id, cb) => {
-  children(branch, id, id => {
-    cb(new Leaf(branch, id), getString(branch.leaves[id].key))
-  })
 }
 
 const map = (branch, id, cb) => {
   const mapped = []
-  children(branch, id, id => {
-    mapped.push(cb(new Leaf(branch, id), getString(branch.leaves[id].key)))
-  })
+  for (const key in branch.leaves[id].keys) {
+    if (branch.leaves[key] !== null) {
+      mapped.push(cb(new Leaf(branch, key), getString(branch.leaves[key].key)))
+    }
+  }
   return mapped
 }
 
 const filter = (branch, id, cb) => {
   const filtered = []
-  children(branch, id, id => {
-    const subLeafInstance = new Leaf(branch, id)
-    if (cb(subLeafInstance, getString(branch.leaves[id].key))) {
-      filtered.push(subLeafInstance)
+  for (const key in branch.leaves[id].keys) {
+    if (branch.leaves[key] !== null) {
+      const subLeafInstance = new Leaf(branch, key)
+      if (cb(subLeafInstance, getString(branch.leaves[key].key))) {
+        filtered.push(subLeafInstance)
+      }
     }
-  })
+  }
   return filtered
 }
 
 const find = (branch, id, cb) => {
-  const found = children(branch, id, id => {
-    return cb(new Leaf(branch, id), getString(branch.leaves[id].key))
-  })
-  if (!Array.isArray(found)) {
-    return new Leaf(branch, found)
+  for (const key in branch.leaves[id].keys) {
+    if (branch.leaves[key] !== null) {
+      const subLeafInstance = new Leaf(branch, key)
+      if (cb(subLeafInstance, getString(branch.leaves[key].key))) {
+        return subLeafInstance
+      }
+    }
   }
 }
 
 const reduce = (branch, id, cb, accumulator) => {
   let skipFirst = accumulator === undefined
-  children(branch, id, id => {
-    if (skipFirst) {
-      accumulator = compute(branch, id)
-      skipFirst = false
-    } else {
-      accumulator = cb(accumulator, new Leaf(branch, id), getString(branch.leaves[id].key))
+  for (const key in branch.leaves[id].keys) {
+    if (branch.leaves[key] !== null) {
+      if (skipFirst) {
+        accumulator = compute(branch, key)
+        skipFirst = null
+      } else {
+        accumulator = cb(accumulator, new Leaf(branch, key), getString(branch.leaves[key].key))
+      }
     }
-  })
+  }
   return accumulator
 }
 
-export { children, forEach, map, filter, find, reduce }
+export { forEach, map, filter, find, reduce }
